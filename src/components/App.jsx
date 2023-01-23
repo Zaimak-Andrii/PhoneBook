4d857858807156b4b206b9d7070628977ad0c4a9
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-01-23 14:53:13 +0200

    Fix page not found

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, useEffect } from 'react';
+import { lazy, useEffect, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useLazyRefreshQuery } from 'services/authAPI';
 import { useSelector } from 'react-redux';
@@ -6,12 +6,13 @@ import { selectIsAuthenticated, selectToken } from 'redux/auth/auth.selectors';
 import { Layout } from './Layout';
 import { PrivatePage, RestrictedPage } from 'pages';
 import { routes } from 'constants/routes';
-import NotFoundPage from 'pages/NotFound.page';
+// import NotFoundPage from 'pages/NotFound.page';
 
 const HomePage = lazy(() => import('pages/Home'));
 const ContactsPage = lazy(() => import('pages/Contacts'));
 const LoginPage = lazy(() => import('pages/Auth/Login.page'));
 const RegistrationPage = lazy(() => import('pages/Auth/Registration.page'));
+const NotFoundPage = lazy(() => import('pages/NotFound.page'));
 
 export const App = () => {
   const token = useSelector(selectToken);
@@ -55,7 +56,14 @@ export const App = () => {
             }
           />
         </Route>
-        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="*"
+          element={
+            <Suspense fallbac={null}>
+              <NotFoundPage />
+            </Suspense>
+          }
+        />
       </Routes>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 export { default as HomePage } from './Home';
+
 export { default as ContactsPage } from './Contacts';
 export { default as LoginPage } from './Auth/Login.page';
 export { default as RegistrationPage } from './Auth/Registration.page';
 export { default as PrivatePage } from './PrivatePage';
 export { default as RestrictedPage } from './RestrictedPage';
+export { default as NotFoundPage } from './NotFound.page';
